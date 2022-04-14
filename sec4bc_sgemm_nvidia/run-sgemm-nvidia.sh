19e0db2925309fdaf21a4bfdc4aeb653b29d43bc
Prasoon Sinha <sinhaprasoon@gmail.com>
2022-04-14 18:47:17 -0500

    Add build and run scripts for sgemm nvidia, update Makefile

---
+++ sec4bc_sgemm_nvidia/run-sgemm-nvidia.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Usage: ./run-sgemm-nvidia.sh
+
+# Parameter to configure - change these as required!
+NUM_KERN=100
+DEVICE_ID=0
+SIZE=25536
+
+echo "Number of kernels: ${NUM_KERN}"
+echo "GPU ID: ${DEVICE_ID}"
+echo "Matrix size: ${SIZE}"
+
+# Get UUID of GPU SGEMM kernels are run on
+UUID_list=(`nvidia-smi -L | awk '{print $NF}' | tr -d '[)]'`)
+UUID=${UUID_list[${1}]}
+echo "UUID: ${UUID}"
+
+# Get timestamp at the time of the run
+ts=`date '+%s'`
+
+# File name
+FILE_NAME=sgemm_${SIZE}_${NUM_KERN}_${UUID}_${DEVICE_ID}_${ts}.csv
+echo "Output file name: ${FILE_NAME}"
+
+# Run application with profiling via nvprof
+__PREFETCH=off nvprof --print-gpu-trace --event-collection-mode continuous --system-profiling on --kernel-latency-timestamps on --csv --log-file ${FILE_NAME} --device-buffer-size 128 --continuous-sampling-interval 1 -f ./sgemm_nvidia ${SIZE} ${NUM_KERN} ${DEVICE_ID}
