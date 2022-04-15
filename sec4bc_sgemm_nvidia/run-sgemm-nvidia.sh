9c09576f3f9f48c5a344c01b3a93a0c7a5b65c0e
Prasoon Sinha <sinhaprasoon@gmail.com>
2022-04-14 19:31:11 -0500

    Minor updates to sgemm nvidia run script

--- sec4bc_sgemm_nvidia/run-sgemm-nvidia.sh
+++ sec4bc_sgemm_nvidia/run-sgemm-nvidia.sh
@@ -14,14 +14,17 @@ echo "Matrix size: ${SIZE}"
 # Get UUID of GPU SGEMM kernels are run on
 UUID_list=(`nvidia-smi -L | awk '{print $NF}' | tr -d '[)]'`)
 UUID=${UUID_list[${1}]}
-echo "UUID: ${UUID}"
+echo "GPU UUID: ${UUID}"
 
 # Get timestamp at the time of the run
 ts=`date '+%s'`
 
 # File name
-FILE_NAME=sgemm_${SIZE}_${NUM_KERN}_${UUID}_${DEVICE_ID}_${ts}.csv
+FILE_NAME=sgemm_nvidia_${SIZE}_${NUM_KERN}_${UUID}_${DEVICE_ID}_${ts}.csv
 echo "Output file name: ${FILE_NAME}"
 
 # Run application with profiling via nvprof
 __PREFETCH=off nvprof --print-gpu-trace --event-collection-mode continuous --system-profiling on --kernel-latency-timestamps on --csv --log-file ${FILE_NAME} --device-buffer-size 128 --continuous-sampling-interval 1 -f ./sgemm_nvidia ${SIZE} ${NUM_KERN} ${DEVICE_ID}
+
+# Move output csv file to ../out/
+mv ${FILE_NAME} ../out/
