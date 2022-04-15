9d9999eee13d5b4262241e2f03e3c9a2f8251c5d
rutwik-n-jain <rutwik.n.jain@gmail.com>
2022-04-15 11:39:21 -0500

    PageRank Dockerfile

--- sec5c_pagerank/run-pagerank.sh
+++ sec5c_pagerank/run-pagerank.sh
@@ -14,11 +14,11 @@ echo $UUID
 ts=`date '+%s'`
 
 echo pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv
-touch ../out/pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv
+touch pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv
 
 echo running PageRank on graph: rajat30.mtx : file_format MMTranspose
 __PREFETCH=off nvprof --print-gpu-trace --event-collection-mode continuous --system-profiling on --kernel-latency-timestamps on --csv --log-file ../out/pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv --device-buffer-size 128 --continuous-sampling-interval 1 -f ${BINDIR}/pagerank_spmv data_dirs/pannotia/pagerank_spmv/data/rajat30/rajat30.mtx 2
 
 echo waitingfor nvprof to flush all data to log 
 wait
-echo end PageRank, profiling log in out/pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv
+echo "end PageRank, now run docker cp containerName:pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv ."
