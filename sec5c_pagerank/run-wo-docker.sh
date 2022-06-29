54a17fc2876921b5edd0d0641b3329a6a56d0c68
Rutwik Jain <rnjain@c008-002.longhorn.tacc.utexas.edu>
2022-06-29 15:02:41 -0500

    Added comment to non docker flow

--- sec5c_pagerank/run-wo-docker.sh
+++ sec5c_pagerank/run-wo-docker.sh
@@ -4,3 +4,6 @@ chmod u+x ./build-pagerank.sh
 chmod u+x ./run-pagerank.sh
 ./build-pagerank.sh
 ./run-pagerank.sh 0 1 0
+
+# run-pagerank.sh ${gpu_num} ${run_num} ${node_num}
+#                 [0/1/2/3]   [user-assigned] [user-assigned, usually used for compute nodes in large clusters]
