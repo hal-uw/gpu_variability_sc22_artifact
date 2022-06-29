fa26fec486023d354abc2ce078cb587c90e96d8e
Rutwik Jain <rnjain@c008-002.longhorn.tacc.utexas.edu>
2022-06-29 14:56:57 -0500

    Updated pagerank non-docker flow

--- sec5c_pagerank/run-pagerank.sh
+++ sec5c_pagerank/run-pagerank.sh
@@ -16,7 +16,7 @@ echo pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv
 touch pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv
 
 echo running PageRank on graph: rajat30.mtx : file_format MMTranspose
-__PREFETCH=off nvprof --print-gpu-trace --event-collection-mode continuous --system-profiling on --kernel-latency-timestamps on --csv --log-file pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv --device-buffer-size 128 --continuous-sampling-interval 1 -f ./pagerank_spmv data_dirs/pannotia/pagerank_spmv/data/rajat30/rajat30.mtx 2
+__PREFETCH=off /usr/local/cuda-10.1/bin/nvprof --print-gpu-trace --event-collection-mode continuous --system-profiling on --kernel-latency-timestamps on --csv --log-file pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv --device-buffer-size 128 --continuous-sampling-interval 1 -f ./pagerank_spmv data_dirs/pannotia/pagerank_spmv/data/rajat30/rajat30.mtx 2
 
 echo waitingfor nvprof to flush all data to log 
 wait
---
+++ sec5c_pagerank/run-wo-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chmod u+x ./build-pagerank.sh
+chmod u+x ./run-pagerank.sh
+./build-pagerank.sh
+./run-pagerank.sh 0 1 0
