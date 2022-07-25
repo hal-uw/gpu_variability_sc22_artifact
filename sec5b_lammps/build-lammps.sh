7528e67fcd2af17872f16948ecc1063ae71febd2
Rutwik Jain <rnjain@c197-072.frontera.tacc.utexas.edu>
2022-07-25 17:34:32 -0500

    Updates for LAMMPS to use Singularity

--- sec5b_lammps/build-lammps.sh
+++ sec5b_lammps/build-lammps.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 cd src
-export CUDA_HOME=/usr/local/cuda-10.1
-export PATH=$CUDA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$CUDA_HOME/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 make clean-all
 make yes-user-reaxc
 make yes-kokkos
