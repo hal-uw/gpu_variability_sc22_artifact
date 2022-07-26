d5e0435837e71471c7103a376e174cbddd367e05
BranTran <45236414+BranTran@users.noreply.github.com>
2022-07-26 09:52:25 -0500

    Update: Lift Imagenet download to datasets.py

--- sec5a_resnet/cnn_utils/datasets.py
+++ sec5a_resnet/cnn_utils/datasets.py
@@ -3,9 +3,55 @@ import kfac
 import torch
 import torch.distributed as dist
 
-from imagenetv2_pytorch import ImageNetV2Dataset
-from torch.utils.data import DataLoader
+#Testing lift of ImageNetV2 Extract
+import pathlib
+import tarfile
+import requests
+import shutil
+from tqdm import tqdm
+from PIL import Image
+
+# from imagenetv2_pytorch import ImageNetV2Dataset
+from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
+from torchvision.datasets import ImageFolder
+
+URLS = {"matched-frequency" : "https://imagenetv2public.s3-us-west-2.amazonaws.com/imagenetv2-matched-frequency.tar.gz",
+        "threshold-0.7" : "https://imagenetv2public.s3-us-west-2.amazonaws.com/imagenetv2-threshold0.7.tar.gz",
+        "top-images": "https://imagenetv2public.s3-us-west-2.amazonaws.com/imagenetv2-top-images.tar.gz",
+        "val": "https://imagenetv2public.s3-us-west-2.amazonaws.com/imagenet_validation.tar.gz"}
+
+FNAMES = {"matched-frequency" : "imagenetv2-matched-frequency-format-val",
+        "threshold-0.7" : "imagenetv2-threshold0.7-format-val",
+        "top-images": "imagenetv2-top-images-format-val",
+        "val": "imagenet_validation"}
+
+V2_DATASET_SIZE = 10000
+def load_imagenet(variant="matched-frequency", location="."):
+#    dataset_root = pathlib.Path(f"{location}/ImageNetV2-{variant}/") #./ImageNetV2-matched-frequency/
+    dataset_root = pathlib.Path(f"{location}/{FNAMES[variant]}/") #./imagenetv2-matched-frequency-format-val
+    tar_root = pathlib.Path(f"{location}/ImageNetV2-{variant}.tar.gz")# ./ImageNetV2-matched-frequency.tar.gz/
+    fnames = list(dataset_root.glob("**/*.jpeg"))
+    assert variant in URLS, f"unknown V2 Variant: {variant}"
+    if not dataset_root.exists() or len(fnames) != V2_DATASET_SIZE:
+        if not tar_root.exists():
+            print(f"Dataset {variant} not found on disk, downloading....")
+            response = requests.get(URLS[variant], stream=True)
+            total_size_in_bytes= int(response.headers.get('content-length', 0))
+            block_size = 1024 #1 Kibibyte
+            progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+            with open(tar_root, 'wb') as f:
+                for data in response.iter_content(block_size):
+                    progress_bar.update(len(data))
+                    f.write(data)
+            progress_bar.close()
+            if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+                assert False, f"Downloading from {URLS[variant]} failed"
+        print("Extracting....")
+        tarfile.open(tar_root).extractall(f"{location}")# tar file gets extracted to ./imagenetv2-matched-frequency-format-val
+        # shutil.move(f"{location}/{FNAMES[variant]}", dataset_root) #Rename from extracted dir to ImageNetV2-matched-frequency
+    else:
+        print("Extracted dataset already exists. Skipping download/extract of dataset.")
 
 def get_cifar(args):
     transform_train = transforms.Compose([
@@ -31,9 +77,10 @@ def get_cifar(args):
 
 
 def get_imagenet(args): 
-    train_dataset = ImageNetV2Dataset("matched-frequency") # supports matched-frequency, threshold-0.7, top-images variants 
+    #train_dataset = ImageNetV2Dataset("matched-frequency") # supports matched-frequency, threshold-0.7, top-images variants 
+    load_imagenet()
     formatted_dataset = datasets.ImageFolder(
-                "./ImageNetV2-matched-frequency",
+                "./imagenetv2-matched-frequency-format-val",
                 transform=transforms.Compose([
                 transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
@@ -53,3 +100,4 @@ def make_sampler_and_loader(args, train_dataset):
             sampler=train_sampler, **kwargs)
 
     return train_sampler, train_loader
+
