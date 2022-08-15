b6ad6ca704479eba577b10418f00785664990559
Rutwik Jain <rnjain@eldin.cs.wisc.edu>
2022-08-15 14:56:23 -0500

    Uncomment singularity pull

--- sec4d_sgemm_amd/sgemm-singularity.sh
+++ sec4d_sgemm_amd/sgemm-singularity.sh
@@ -6,7 +6,7 @@ echo "Pulling container for $uarch"
 if [[ "$uarch" == *ppc64le* ]]; then
     echo "Base image (sunway513/rocm-dev) only supports AMD64/x86_64  architectures. Aborting." 
 else
-    #singularity pull docker://sunway513/rocm-dev:4.5.2-rocblas-bench
+    singularity pull docker://sunway513/rocm-dev:4.5.2-rocblas-bench
     singularity run --nv docker://sunway513/rocm-dev:4.5.2-rocblas-bench ./build-sgemm-amd.sh
     sleep 30
     singularity run --nv docker://sunway513/rocm-dev:4.5.2-rocblas-bench ./run-sgemm-amd.sh
