770f409ff4b419f75471101926a077bbfe2e85a9
Rutwik Jain <rnjain@c199-022.frontera.tacc.utexas.edu>
2024-03-05 10:40:05 -0600

    PageRank updates on Frontera

--- sec5d_pagerank/run-pagerank.sh
+++ sec5d_pagerank/run-pagerank.sh
@@ -2,6 +2,9 @@
 
 # run-pagerank.sh $gpu_num $num_run $node
 
+module load tacc-apptainer
+module load cuda
+
 export CUDA_HOME=/usr/local/cuda-10.1 
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
@@ -22,17 +25,7 @@ touch pagerank_${UUID}_run${2}_node_${HOSTNAME}_gpu${1}_${ts}.csv
 #echo running PageRank on graph: rajat30.mtx : file_format MMTranspose
 echo running PageRank on graph: pre2.mtx : file_format MMTranspose
 
-#if [ -d "/usr/local/cuda-10.1/bin" ]
-#then
-#    __PREFETCH=off /usr/local/cuda-10.1/bin/nvprof --print-gpu-trace --event-collection-mode continuous --system-profiling on --kernel-latency-timestamps on --csv --log-file pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv --device-buffer-size 128 --continuous-sampling-interval 1 -f ./pagerank_spmv data_dirs/pannotia/pagerank_spmv/data/rajat30/rajat30.mtx 2
-#    echo waitingfor nvprof to flush all data to log 
-#    wait
-#    echo "Completed PageRank. Output in pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv"
-#elif [ -d "/usr/local/cuda/bin" ]
-#then
-    #__PREFETCH=off /usr/local/cuda/bin/nvprof --print-gpu-trace --event-collection-mode continuous --system-profiling on --kernel-latency-timestamps on --csv --log-file pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv --device-buffer-size 128 --continuous-sampling-interval 1 -f ./pagerank_spmv data_dirs/pannotia/pagerank_spmv/data/rajat30/rajat30.mtx 2
-#nv-nsight-cu-cli --metrics gpu__time_duration.avg --csv ./pagerank_spmv data_dirs/pannotia/pagerank_spmv/data/rajat30/rajat30.mtx 2 | grep '^"' > pagerank_${UUID}_run${2}_node${3}_gpu${1}_${ts}.csv
-nv-nsight-cu-cli --metrics gpu__time_duration.avg --csv ./pagerank_spmv data_dirs/pannotia/pagerank_spmv/data/pre2/pre2.mtx 2 | grep '^"' > pagerank_${UUID}_run${2}_node_${HOSTNAME}_gpu${1}_${ts}.csv
+ncu --metrics gpu__time_duration.avg --csv ./pagerank_spmv data_dirs/pannotia/pagerank_spmv/data/coPapersDBLP/coPapersDBLP.mtx 2 | grep '^"' > pagerank_${UUID}_run${2}_node_${HOSTNAME}_gpu${1}_${ts}.csv
 echo waitingfor nvprof to flush all data to log 
 wait
 echo "Completed PageRank. Output in pagerank_${UUID}_run${2}_node_${HOSTNAME}_device${1}_${ts}.csv"
